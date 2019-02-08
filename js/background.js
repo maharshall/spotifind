09f92dbe68f9dde6f6e90b018ee8d19a3ba6ca82
maharshall <maharshall@gmail.com>
2019-02-08 16:17:02 -0500

    Can search for selected text in Spotify web player

--- js/background.js
+++ js/background.js
@@ -1,11 +1,11 @@
 chrome.runtime.onInstalled.addListener(function() {
     chrome.contextMenus.create({
         id: "search",
-        title: "Search...",
+        title: "Search Spotify for \"%s\"",
         contexts: ["selection"]
   });
 });
 
-chrome.contextMenus.onClicked.addListener(clickHandler = function() {
-    alert("I have been clicked");
+chrome.contextMenus.onClicked.addListener(clickHandler = function(info) {
+    window.open('https://open.spotify.com/search/results/'+info.selectionText.toLowerCase(), '_blank');
 });
